701d2d7418daf6a89d2b324fb9b4ae207170ed7d
Park Taeseon <wish_snow@naver.com>
2016-11-18 19:39:14 +0900

    2nd commit

--- src/com/silentpeople/exam1/ForWhile.java
+++ src/com/silentpeople/exam1/ForWhile.java
@@ -6,16 +6,16 @@ public class ForWhile {
 		// TODO Auto-generated method stub
 		for(int i = 0 ; i < 5 ; i++){
 			for(int j = 0 ; j < i ; j++)
-				System.out.print("  ");
+				System.out.print(" ");
 			for(int j = 0 ; j < 9 - 2 * i ; j++)
-				System.out.print("* ");
+				System.out.print("*");
 			System.out.println();
 		}
 		for(int i = 4 ; i > 0 ; i--){
 			for(int j = 0 ; j < i-1 ; j++)
-				System.out.print("  ");
+				System.out.print(" ");
 			for(int j = 0 ; j < 11 - 2 * i ; j++)
-				System.out.print("* ");
+				System.out.print("*");
 			System.out.println();
 		}
 	}
